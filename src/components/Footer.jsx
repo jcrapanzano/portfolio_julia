210219b330380f52fb640770ffd67adbdb21516d
Julia CRAPANZANO <julia.crapanzano@gmail.com>
2025-10-27 18:25:27 +0100

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-pink-100 border-t mt-12 py-6 text-center text-sm text-gray-600">
-      © {new Date().getFullYear()} Julia — UX/UI Designer. Tous droits réservés.
-    </footer>
-  );
-}
